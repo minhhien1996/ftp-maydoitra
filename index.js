5a7fa43e13704b3d9564e777fd4c4813bde66bec
Hien Do <hien.do@employmenthero.com>
2021-10-30 12:40:20 +0700

    make index executable

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+#!/Users/minhhien1996/.nvm/versions/node/v12.22.4/bin/node
 const cheerio = require('cheerio');
 const url = require('url');
 const axios = require('axios');
+const {exec} = require("child_process");
 
 const main = async () => {
-  // const url = "https://fptshop.com.vn/may-doi-tra/Ajax/Product/AjaxGetAllProductByNameAscii?nameascii=iphone-12-pro-max-128gb&currentPage=0&OrderBy=1&FromPrice=&ToPrice=&CityID=23"
-  const url = "https://fptshop.com.vn/may-doi-tra/Ajax/Product/AjaxGetAllProductByNameAscii?nameascii=iphone-12-pro-max-128gb&currentPage=0&OrderBy=1&FromPrice=&ToPrice=&CityID=26"
+  console.log(`CHECK FPT AT ${new Date()}`)
+  // HCM
+  const url = "https://fptshop.com.vn/may-doi-tra/Ajax/Product/AjaxGetAllProductByNameAscii?nameascii=iphone-12-pro-max-128gb&currentPage=0&OrderBy=1&FromPrice=&ToPrice=&CityID=23"
+  // HN
+  // const url = "https://fptshop.com.vn/may-doi-tra/Ajax/Product/AjaxGetAllProductByNameAscii?nameascii=iphone-12-pro-max-128gb&currentPage=0&OrderBy=1&FromPrice=&ToPrice=&CityID=26"
   const host = "https://fptshop.com.vn/";
   const response = await axios.get(url);
   if (!response) {
@@ -14,11 +19,13 @@ const main = async () => {
   const html = response.data;
   const $ = cheerio.load(html)
   const links = $('.mc-lpcol')
-  links.each(function() {
+  links.each(function () {
     const href = $(this).find('a').attr('href');
     const text = $(this).find('a').text().replace(/\s\s+/g, ' ').trim();
-    console.log(text, new URL(href, host).href)
-  })
+    const fullUrl = new URL(href, host).href
+    console.log(text, fullUrl)
+    exec(`open ${fullUrl}`)
+  });
 }
 
 
